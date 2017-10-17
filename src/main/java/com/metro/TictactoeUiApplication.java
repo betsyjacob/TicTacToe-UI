8f8342c69712d5c64d5a45763253f637ffa6517f
betsyjacob <betssyyy@gmail.com>
2017-10-17 11:21:50 +0400

    Add files via upload

---
+++ src/main/java/com/metro/TictactoeUiApplication.java
@@ -0,0 +1,12 @@
+package com.metro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeUiApplication.class, args);
+	}
+}
